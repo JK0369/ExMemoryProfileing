ee7115195b13ec17e1ba70f99d4c06ea81ac80d0
jake <palatable7@naver.com>
2023-12-10 18:47:34 +0900

    Add

--- ExImageIO/ViewController.swift
+++ ExImageIO/ViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import ImageIO
 
 class ViewController: UIViewController {
     private let imageView: UIImageView = {
@@ -13,6 +14,8 @@ class ViewController: UIViewController {
         view.translatesAutoresizingMaskIntoConstraints = false
         return view
     }()
+    
+    let url = URL(string: "https://cdn.pixabay.com/photo/2023/11/30/07/51/bridge-8420945_1280.jpg")!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,15 +26,41 @@ class ViewController: UIViewController {
             imageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
         ])
         
-        guard let url = URL(string: "https://cdn.pixabay.com/photo/2023/11/30/07/51/bridge-8420945_1280.jpg") else { return }
+        downloadWithResizeV1()
+    }
+    
+    func downloadWithResizeV1() {
         downloadImageData(from: url) { [weak self] data in
-            guard let data else { return }
+            guard let data, let image = UIImage(data: data) else { return }
             DispatchQueue.main.async {
-                self?.imageView.image = UIImage(data: data)
+                self?.imageView.image = image
+            }
+            
+            // resizing image
+            let scale = 0.2
+            let size = CGSize(width: image.size.width * scale, height: image.size.height * scale)
+            let renderer = UIGraphicsImageRenderer(size: size)
+            let renderedImage = renderer.image { _ in
+                image.draw(in: .init(origin: .zero, size: size))
+            }
+            DispatchQueue.main.async {
+                self?.imageView.image = renderedImage
             }
         }
     }
     
+    func downloadWithResizeV2() {
+        let imageSource = CGImageSourceCreateWithURL(url as CFURL, nil)!
+        let properties = CGImageSourceCopyPropertiesAtIndex(imageSource, 0, nil)
+        let options: [CFString : Any] = [
+            kCGImageSourceThumbnailMaxPixelSize: 100, 
+            kCGImageSourceCreateThumbnailFromImageAlways: true
+        ]
+        let scaledImage = CGImageSourceCreateThumbnailAtIndex(imageSource, 0, options)
+    }
+}
+
+private extension ViewController {
     func downloadImageData(from url: URL, completion: @escaping (Data?) -> Void) {
         URLSession.shared.dataTask(with: url) { data, response, error in
             guard let data = data, error == nil else {
